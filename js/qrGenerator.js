ac7f3230b690547630032d998260ac222340bd20
Matias Blanco <matias.david.blanco@gmail.com>
2023-01-20 11:18:20 -0300

    Creando funcion share

--- js/qrGenerator.js
+++ js/qrGenerator.js
@@ -31,4 +31,35 @@ btn_download.addEventListener("click", () => {
 
 });
 
+
+// btn_share.addEventListener("click", () => {
+//     btn_share.href = `href="whatsapp://send?text=https://elcssar.com`;
+// })
+
+//Creamos una función que se ejecutará cuando el usuario haga click en el botón
+btn_share.addEventListener("click", (event) => {
+ 
+  // Verificamos si el navegador tiene soporte para el API compartir
+  if ("share" in navigator) {
+    navigator
+      .share({
+        // Defino un título para la ventana de compartir
+        title: "Comparte Esta Página en Tu Plataforma Favorita",
+ 
+        // Detecto la URL actual de la página 
+        url:
+            window.location.href
+      })
+ 
+      // Mensaje en Consola cuando se presiona el botón de compartir 
+      .then(() => {
+        console.log("Contenido Compartido !");
+      })
+      .catch(console.error);
+  } else {
+    // Si el navegador no tiene soporte para la API compartir, le enviamos un mensaje al usuario
+    alert('Lo siento, este navegador no tiene soporte para recursos compartidos.')
+  }
+});
+
 // crear funcion para compartir qr
