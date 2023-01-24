e07a3d5bb8fa633ea365b504e2f819355b05b2e6
Matias Blanco <matias.david.blanco@gmail.com>
2023-01-24 17:41:21 -0300

    Actualizando menu hamburguesa

--- js/hamburger_menu.js
+++ js/hamburger_menu.js
@@ -2,7 +2,13 @@ const btn = document.getElementById("hamburger_btn");
 const panel = document.querySelector(".panel");
 
 const desplegarMenu = () => {
-    panel.classList.toggle("is-active")
+    panel.classList.toggle("is-active");
+    btn.classList.toggle("is-active");
 }
 
 btn.addEventListener("click",desplegarMenu);
+
+const cerrarMenu = () => {
+    panel.classList.toggle("is-active");
+    btn.classList.toggle("is-active");
+}
